de5e058407bcdded595f045dd1e2815543921fd1
PrinceSP <princepasombaran@gmail.com>
2025-03-27 17:56:19 +0700

    feat: create interview card item

---
+++ components/interviewCard.tsx
@@ -0,0 +1,49 @@
+import { Feedback, InterviewCardProps } from '@/types'
+import React from 'react'
+import Image from 'next/image'
+import { getRandomInterviewCover, localDefault } from '@/lib/utils'
+import { Button } from './ui/button'
+import Link from 'next/link'
+import TechIcons from './techIcons'
+
+const InterviewCard = ({ interviewId, createdAt, userId, role, techstack, type }: InterviewCardProps) => {
+  const feedback = null as Feedback | null
+  const normalise = /mix/gi.test(type) ? "Mixed" : type
+
+  return (
+    <div className='card-border w-[360px] max-sm:w-full min-h-96'>
+      <div className="card-interview">
+        <div>
+          <div className={`absolute top-0 right-0 w-fit px-4 py-2 rounded-bl-lg ${normalise == "Mixed" ? "bg-light-600" : "bg-dark-300"}`}>
+            <p className="badge-text">{normalise}</p>
+          </div>
+          <Image src={getRandomInterviewCover()} alt="cover image" width={90} height={90} className="rounded-full object-fit size-[90px]" />
+          <h3 className="mt-5 capitalize">{role} Interview</h3>
+          <div className="flex items-center justify-between">
+            <div className="flex">
+              <Image src="/calendar.svg" alt="calendar icon" width={22} height={22} />
+              <p>{localDefault(createdAt)}</p>
+            </div>
+
+            <div className="flex">
+              <Image src="/star.svg" alt="star icon" width={22} height={22} />
+              <p>{feedback?.totalScore || "---"}/100</p>
+            </div>
+          </div>
+
+          <p className="line-clamp-2 mt-5">{feedback?.finalAssessment || "You haven't taken the interview yet. Take it now to improve your skills."}</p>
+        </div>
+        <div className="flex justify-between">
+          <TechIcons techStack={techstack} />
+          <Button className="btn-primary">
+            <Link href={feedback ? `/interview/${interviewId}/feedback` : `/interview/${interviewId}/`}>
+              {feedback ? "Check Feedback" : "View Interview"}
+            </Link>
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default InterviewCard
