fffe07dc96a349c459498d96c62299d11b9c561c
PrinceSP <princepasombaran@gmail.com>
2025-03-28 19:21:23 +0700

    feat: add function for user authentication

--- components/authForm.tsx
+++ components/authForm.tsx
@@ -10,6 +10,9 @@ import { Button } from "@/components/ui/button"
 import { Form } from "@/components/ui/form"
 import { FormType } from "@/types"
 import { useRouter } from "next/navigation";
+import { auth } from "@/firebase/client"
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth"
+import { signIn, signUp } from "@/lib/actions/auth.action"
 
 const authSchema = (type: FormType) => {
   return z.object({
@@ -31,17 +34,64 @@ const AuthForm = ({ type }: { type: FormType }) => {
     },
   });
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  async function onSubmit(values: z.infer<typeof formSchema>) {
     try {
       if (type === "sign-up") {
-        toast.success("Account created successfully. Please sign in.")
+        // const { name, email, password } = values
+        // const userCredentials = await createUserWithEmailAndPassword(auth, email, password)
+        
+        // const result = await signUp({
+        //   uid: userCredentials.user.uid, name: name!, email, password
+        // })
+        
+        // if (!result.success) {
+        //   toast.error(result.message)
+        //   return;
+        // }
+        
+        // toast.success("Account created successfully. Please sign in.")
+        // router.push("/sign-in");
+        const { name, email, password } = values;
+
+        const userCredential = await createUserWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+
+        const result = await signUp({
+          uid: userCredential.user.uid,
+          name: name!,
+          email,
+          password,
+        });
+
+        if (!result.success) {
+          toast.error(result.message);
+          return;
+        }
+
+        toast.success("Account created successfully. Please sign in.");
         router.push("/sign-in");
       }
-      else{
+      else {
+        const { email, password } = values
+        const userCredential = await signInWithEmailAndPassword(auth, email, password)
+
+        const idToken = await userCredential.user.getIdToken()
+
+        if (!idToken) {
+          toast.error("Sign in failed")
+          return
+        }
+
+        await signIn({ email, idToken })
+
         toast.success("Signed in successfully.");
         router.push("/");
       }
     } catch (error) {
+      // console.log(error)
       toast.error("Wrong username or password!")
     }
   }
