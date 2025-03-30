870641513f5d16b41b7d2c637303e7ed94ef9748
PrinceSP <princepasombaran@gmail.com>
2025-03-30 07:22:54 +0700

    feat: create interview app UI

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,13 @@
+import Agent from '@/components/agent'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <>
+      <h3>Interview Generation</h3>
+      <Agent userName='Hello' type='generate'/>
+    </>
+  )
+}
+
+export default Page
